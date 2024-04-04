488757d2d880739e0e14b10fb2737acbfd7782e3
vinnie <githinjigicheru146@gmail.com>
2024-04-04 08:04:26 +0300

    reverse twice to reverse a sentence

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,16 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    #words = sentence.split
+    # reversed_words = []
+    # words.each do |word|
+    #     #push each reversed word to the reversed_words array
+    #     reversed_words << word.reverse
+    # end
+    # reversed_sentence = reversed_words.reverse.join(" ")
+    
+    reversed_sentence = sentence.split.map(&:reverse).reverse.join(" ")
+    binding.pry
+end
+
+reverse_each_word("Vincent")
